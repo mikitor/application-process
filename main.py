cfe06277d6faed4c8b1ca8283513008efd393819
Nick Torbich <nicktorbich@protonmail.com>
2017-05-15 23:36:16 +0200

    created data manager module

---
+++ main.py
@@ -0,0 +1,11 @@
+import psycopg2
+from data_manager import access_db
+
+
+@access_db
+def full_name_mentors():
+    query_command = "SELECT first_name, last_name FROM mentors;"
+    return query_command
+
+if __name__ == '__main__':
+    print(full_name_mentors())
