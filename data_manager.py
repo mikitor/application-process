b5d4fa396505eebfcd186703039c875d766c1598
Nick Torbich <nicktorbich@protonmail.com>
2017-05-16 00:47:36 +0200

    deleted too simple comments

--- data_manager.py
+++ data_manager.py
@@ -4,13 +4,9 @@ import psycopg2
 def access_db(some_function):
     def wrapper():
         try:
-            # setup connection string
             connect_str = "dbname='miki' user='miki' host='localhost' password='123456789gl'"
-            # use our connection values to establish a connection
             conn = psycopg2.connect(connect_str)
-            # set autocommit option, to do every query when we call it
             conn.autocommit = True
-            # create a psycopg2 cursor that can execute queries
             cursor = conn.cursor()
 
             query_command = some_function()
