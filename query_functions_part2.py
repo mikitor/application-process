f97ea380f6f78d0d7971b8400b8c9bfe02f8876f
Nick Torbich <nicktorbich@protonmail.com>
2017-05-25 23:02:14 +0200

    Created SQL query for /all-school page

--- query_functions_part2.py
+++ query_functions_part2.py
@@ -8,3 +8,12 @@ def mentors():
                      USING (city) \
                      ORDER BY mentors.id;"
     return query_command
+
+
+@access_db
+def all_school():
+    query_command = "SELECT mentors.first_name, mentors.last_name, schools.name, schools.country \
+                     FROM mentors RIGHT OUTER JOIN schools \
+                     USING (city) \
+                     ORDER BY mentors.id;"
+    return query_command
