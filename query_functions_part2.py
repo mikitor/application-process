28ba6fb5cb6d2aa6713ff7ccbc26770de9c9a219
Nick Torbich <nicktorbich@protonmail.com>
2017-05-25 22:52:01 +0200

    Created SQL query for /mentors page

---
+++ query_functions_part2.py
@@ -0,0 +1,10 @@
+from data_manager import access_db
+
+
+@access_db
+def mentors():
+    query_command = "SELECT mentors.first_name, mentors.last_name, schools.name, schools.country \
+                     FROM mentors LEFT OUTER JOIN schools \
+                     USING (id) \
+                     ORDER BY mentors.id;"
+    return query_command
