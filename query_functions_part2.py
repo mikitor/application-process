02e83b2eb49e4cf4d85421e6ee6d8cffba4b419a
Nick Torbich <nicktorbich@protonmail.com>
2017-05-27 19:40:07 +0200

    Concatenated the mentor first and last name into one column

--- query_functions_part2.py
+++ query_functions_part2.py
@@ -3,7 +3,7 @@ from data_manager import access_db
 
 @access_db
 def mentors():
-    query_command = "SELECT mentors.first_name, mentors.last_name, schools.name, schools.country \
+    query_command = "SELECT CONCAT(mentors.first_name, ' ', mentors.last_name) AS mentor_name, schools.name, schools.country \
                      FROM mentors LEFT OUTER JOIN schools \
                      USING (city) \
                      ORDER BY mentors.id;"
