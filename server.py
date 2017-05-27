b4dd90d5d036514adb126773dd600dadbdd6c8b4
Nick Torbich <nicktorbich@protonmail.com>
2017-05-27 18:47:53 +0200

    Finished basic routing of the pages

--- server.py
+++ server.py
@@ -1,4 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
+import psycopg2
+import query_functions_part2
 
 
 app = Flask(__name__)
@@ -6,38 +8,44 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    pass
+    return render_template('layout.html')
 
 
-@app.route('mentors')
+@app.route('/mentors')
 def mentors():
-    pass
+    table = query_functions_part2.mentors()
+    return render_template('table.html', table=table)
 
 
-@app.route('all-school')
+@app.route('/all-school')
 def all_school():
-    pass
+    table = query_functions_part2.all_school()
+    return render_template('table.html', table=table)
 
 
 @app.route('/mentors-by-country')
 def mentors_by_country():
-    pass
+    table = query_functions_part2.mentors_by_country()
+    return render_template('table.html', table=table)
 
 
 @app.route('/contacts')
 def contacts():
-    pass
+    table = query_functions_part2.contacts()
+    return render_template('table.html', table=table)
 
 
 @app.route('/applicants')
 def applicants():
-    pass
+    table = query_functions_part2.applicants()
+    return render_template('table.html', table=table)
 
 
 @app.route('/applicants-and-mentors')
 def applicants_and_mentors():
-    pass
+    table = query_functions_part2.applicants_and_mentors()
+    return render_template('table.html', table=table)
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
